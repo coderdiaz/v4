f79356a869c7727d99dd8225b8f9bc5215b9cb63
Javier Diaz <coderdiaz@gmail.com>
2020-08-21 14:47:40 -0500

    feat: added container component

---
+++ components/Container.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+interface IContainerProps {
+  children?: React.ReactNode;
+}
+
+const Container = ({ children }: IContainerProps) => {
+  return <div className="container mx-auto max-w-5xl px-6 md:px-8 lg:px-6">
+    {children}
+  </div>
+}
+
+export default Container;
