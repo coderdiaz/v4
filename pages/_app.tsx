4438181965a26c851c6e91fd79aa1e55c9315cd0
Javier Diaz <coderdiaz@gmail.com>
2020-08-22 02:14:35 -0500

    feat: configured google analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,16 @@
 import 'tailwindcss/tailwind.css';
 import type { AppProps } from 'next/app';
+import { useEffect } from 'react';
+import * as galite from 'ga-lite';
 
 function MyApp({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    if (process.env.NODE_ENV !== 'development') {
+      galite('create', 'UA-169353371-1', 'auto');
+      galite('send', 'pageview');
+    }
+  }, []);
+
   return <Component {...pageProps} />
 }
 
