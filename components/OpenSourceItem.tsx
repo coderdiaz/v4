bdb5e4f5cdfe09df662392c1009a56d70e96b929
Javier Diaz <coderdiaz@gmail.com>
2020-08-22 18:15:33 -0500

    fix: removed technology from oss project

--- components/OpenSourceItem.tsx
+++ components/OpenSourceItem.tsx
@@ -5,15 +5,11 @@ interface IOpenSourceItemProps {
 }
 
 const OpenSourceItem = ({ item }: IOpenSourceItemProps) => {
-  return <a href={item.uri} className="px-4 py-2 grid lg:grid-cols-12 gap-1 lg:gap-0 items-center cursor-pointer hover:bg-gray-50 dark-hover:bg-dark-700 rounded mb-2 transition duration-300 ease-in-out">
-    <div className="lg:col-span-3 font-semibold inline-block md:mb-0 dark:text-gray-200">
+  return <a href={item.uri} className="px-4 py-2 flex flex-col md:flex-row items-start md:justify-between md:items-center cursor-pointer hover:bg-gray-50 dark-hover:bg-dark-700 rounded mb-2 transition duration-300 ease-in-out">
+    <div className="font-semibold inline-block dark:text-gray-200 mb-1 md:mb-0">
       <span className="inline-flex mr-1">{item.icon}</span> {item.name}
     </div>
-    <p className="lg:col-span-7 text-gray-600 dark:text-gray-600">{item.description}</p>
-    <div className="hidden lg:col-span-2 lg:flex items-center justify-start">
-      <div className={`w-3 h-3 rounded-full mr-2 ${item.tech.color}`} />
-      <span className="inline-flex text-base text-gray-600">{item.tech.name}</span>
-    </div>
+    <p className="lg:w-125 text-gray-600 dark:text-gray-600">{item.description}</p>
   </a>
 };
 
