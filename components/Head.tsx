0bee45e067f77453ddb2cdd6a2e11d5da98ec45f
Javier Diaz <coderdiaz@gmail.com>
2020-08-22 02:40:46 -0500

    feat: added preconnect to ga

--- components/Head.tsx
+++ components/Head.tsx
@@ -23,6 +23,7 @@ const Head = ({ children, meta = {} }: IHeadProps) => {
       <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
       <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
       <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+      <link rel="preconnect" href="https://www.google-analytics.com" />
       <meta charSet="UTF-8" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
       <meta httpEquiv="x-ua-compatible" content="ie=edge" />
