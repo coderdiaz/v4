2e59bba8550d4e551fd66a12f53816787d1424c7
Javier Diaz <coderdiaz@gmail.com>
2020-08-21 20:11:56 -0500

    fix: removed grayscale

--- components/Shots.tsx
+++ components/Shots.tsx
@@ -9,7 +9,7 @@ const Shots = ({ shots }: IShots) => {
     {shots.map(shot => <article className="relative rounded-lg pb-56 lg:pb-85 border border-gray-100 shadow-md transform lg:hover:-translate-y-1 transition-transform duration-300 overflow-hidden" key={shot.id}>
       <a className="inline-block" href={shot.html_url}>
         <img
-          className="absolute top-0 left-0 object-cover w-full h-full rounded-lg lg:grayscale-1 lg:hover:grayscale-none transition-all duration-300 ease-in-out"
+          className="absolute top-0 left-0 object-cover w-full h-full rounded-lg"
           src={shot.images.two_x}
           aria-label={shot.title} />
       </a>
