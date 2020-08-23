dc262b84081f408316de5152b3b0211d50d28b11
Javier Diaz <coderdiaz@gmail.com>
2020-08-23 18:39:21 -0500

    build(next): added support for mdx in nextjs

--- next.config.js
+++ next.config.js
@@ -1,3 +1,14 @@
 const withImages = require('next-images');
+const withMdxEnhanced = require('next-mdx-enhanced');
+const mdxPrism = require('mdx-prism');
 
-module.exports = withImages({ /** Custom configuration for NextJS */});
+module.exports = withImages(withMdxEnhanced({
+  layoutPath: 'layouts',
+  defaultLayout: true,
+  remarkPlugins: [
+    require('remark-autolink-headings'),
+    require('remark-slug'),
+    require('remark-code-titles'),
+  ],
+  rehypePlugins: [mdxPrism],
+})());
