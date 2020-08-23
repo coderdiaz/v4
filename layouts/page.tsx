f3082afeffd9eccf6468f6d466551a44dc8c525c
Javier Diaz <coderdiaz@gmail.com>
2020-08-23 18:39:45 -0500

    feat: added layout to mdx content

---
+++ layouts/page.tsx
@@ -0,0 +1,26 @@
+import Head from '../components/Head';
+import Header from '../components/Header';
+import Footer from '../components/Footer';
+import importScript from '../components/import-script';
+import PageContainer from '../components/PageContainer';
+
+export default function Layout(frontMatter) {
+  return ({ children: content }) => {
+    importScript('/js/prevent.flash.js');
+
+    return (
+      <>
+        <Head meta={{
+          title: frontMatter.title
+        }} />
+        <Header/>
+        <main>
+          <PageContainer>
+            {content}
+          </PageContainer>
+        </main>
+        <Footer />
+      </>
+    )
+  }
+}
