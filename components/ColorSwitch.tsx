f87a82111cfecef2ddbc5f70b19f927059e05e0a
Javier Diaz <coderdiaz@gmail.com>
2020-08-23 02:08:32 -0500

    fix: fixed issue with the svg components

--- components/ColorSwitch.tsx
+++ components/ColorSwitch.tsx
@@ -1,14 +1,13 @@
+import dynamic from 'next/dynamic';
 import useDarkMode from 'use-dark-mode';
+const SunIcon = dynamic(() => import('./SunIcon'), { ssr: false }) ;
+const MoonIcon = dynamic(() => import('./MoonIcon'), { ssr: false }) ;
 
 const ColorSwitch = () => {
-  const darkMode = useDarkMode(false);
+  const { value, toggle } = useDarkMode(false);
 
-  return <button onClick={darkMode.toggle} className="inline-flex items-center text-dark-500 hover:text-dark-800 md:bg-gray-50 md:hover:bg-gray-100 dark:text-white dark-hover:text-white md:dark:bg-dark-500 md:dark-hover:bg-dark-700 rounded p-3 transition duration-300 ease-in-out fill-current outline-none shadow-none" aria-label="Dark Mode">
-    { darkMode.value ? <svg viewBox="0 0 20 20" fill="currentColor" className="w-4 h-4 inline-block align-middle flex-shrink-0">
-      <path fillRule="evenodd" d="M10 2a1 1 0 011 1v1a1 1 0 11-2 0V3a1 1 0 011-1zm4 8a4 4 0 11-8 0 4 4 0 018 0zm-.464 4.95l.707.707a1 1 0 001.414-1.414l-.707-.707a1 1 0 00-1.414 1.414zm2.12-10.607a1 1 0 010 1.414l-.706.707a1 1 0 11-1.414-1.414l.707-.707a1 1 0 011.414 0zM17 11a1 1 0 100-2h-1a1 1 0 100 2h1zm-7 4a1 1 0 011 1v1a1 1 0 11-2 0v-1a1 1 0 011-1zM5.05 6.464A1 1 0 106.465 5.05l-.708-.707a1 1 0 00-1.414 1.414l.707.707zm1.414 8.486l-.707.707a1 1 0 01-1.414-1.414l.707-.707a1 1 0 011.414 1.414zM4 11a1 1 0 100-2H3a1 1 0 000 2h1z" clipRule="evenodd" />
-    </svg> : <svg className="w-4 h-4 inline-block align-middle flex-shrink-0" aria-hidden="true" viewBox="0 0 24 24">
-      <path fill="currentColor" d="M21.4,13.7C20.6,13.9,19.8,14,19,14c-5,0-9-4-9-9c0-0.8,0.1-1.6,0.3-2.4c0.1-0.3,0-0.7-0.3-1 c-0.3-0.3-0.6-0.4-1-0.3C4.3,2.7,1,7.1,1,12c0,6.1,4.9,11,11,11c4.9,0,9.3-3.3,10.6-8.1c0.1-0.3,0-0.7-0.3-1 C22.1,13.7,21.7,13.6,21.4,13.7z" />
-    </svg>}
+  return <button onClick={toggle} className="inline-flex items-center text-dark-500 hover:text-dark-800 md:bg-gray-50 md:hover:bg-gray-100 dark:text-white dark-hover:text-white md:dark:bg-dark-500 md:dark-hover:bg-dark-700 rounded p-3 transition duration-300 ease-in-out fill-current outline-none shadow-none" aria-label="Dark Mode">
+    { value ? <SunIcon /> : <MoonIcon />}
   </button>
 }
 
---
+++ components/SunIcon.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+function SunIcon() {
+  return (
+    <svg className="w-4 h-4 inline-block align-middle flex-shrink-0" viewBox="0 0 24 24" focusable="false" role="presentation" aria-hidden="true">
+      <g stroke-linejoin="full" stroke-linecap="full" stroke-width="2" fill="none" stroke="currentColor">
+        <circle cx="12" cy="12" r="5"></circle>
+        <path d="M12 1v2"></path>
+        <path d="M12 21v2"></path>
+        <path d="M4.22 4.22l1.42 1.42"></path>
+        <path d="M18.36 18.36l1.42 1.42"></path>
+        <path d="M1 12h2"></path>
+        <path d="M21 12h2"></path>
+        <path d="M4.22 19.78l1.42-1.42"></path>
+        <path d="M18.36 5.64l1.42-1.42"></path>
+      </g>
+    </svg>
+  );
+}
+
+export default SunIcon;
