7d16ba093a1c281ce7af53c8dfc7518054577800
Javier Diaz <coderdiaz@gmail.com>
2020-08-28 18:38:03 -0500

    fix: fixed margin

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -10,7 +10,7 @@ const Footer = () => {
     <Container>
       <div className="flex justify-between items-center">
         <Link href="/">
-          <a className="flex flex-row items-center fill-current transition duration-300 ease-in-out">
+          <a className="flex flex-row items-center fill-current transition duration-300 ease-in-out mr-6">
             <svg className="hidden w-6 h-6" viewBox="0 0 72.28 75.44">
               <path d="M46.08,5.29,17.65,12.51a19.57,19.57,0,0,0-14.75,19V51.18A19.56,19.56,0,0,0,22.47,70.74H49.82A19.56,19.56,0,0,0,69.38,51.18V23.4A18.69,18.69,0,0,0,46.08,5.29Zm.62,42.14L35.43,59.21a5.36,5.36,0,0,1-7.57.17h0a5.38,5.38,0,0,1-.17-7.58l7.56-7.9a5.35,5.35,0,0,0-.16-7.57l-7.91-7.56A5.35,5.35,0,0,1,27,21.2h0A5.36,5.36,0,0,1,34.58,21L46.36,32.29,50.23,36a5.36,5.36,0,0,1,.17,7.57Z" fillRule="evenodd"/>
             </svg>
