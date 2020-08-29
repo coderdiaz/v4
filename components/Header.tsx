912f0fd7226d92e1b673d86aca620af70ba45ee0
Javier Diaz <coderdiaz@gmail.com>
2020-08-28 19:59:24 -0500

    fix: fixed active language

--- components/Header.tsx
+++ components/Header.tsx
@@ -26,12 +26,12 @@ const Header = ({ language }: HeaderProps) => {
         <ul className="hidden md:flex justify-start items-center">
           <li className="items-center">
             <Link href="/">
-              <a className="inline-block text-dark-800 hover:text-primary-900 hover:bg-gray-50 dark:text-gray-500 dark-hover:text-white dark-hover:bg-dark-600 rounded font-bold px-2 py-1 transition duration-300 ease-in-out">ES</a>
+              <a className={`inline-block hover:text-primary-900 hover:bg-gray-50 dark-hover:text-white dark-hover:bg-dark-600 rounded font-bold px-2 py-1 transition duration-300 ease-in-out ${language === 'es' ? 'text-primary-900 dark:text-primary-400' : 'text-dark-800 dark:text-gray-500'}`}>ES</a>
             </Link>
           </li>
           <li className="items-center">
             <Link href="/en">
-              <a className="inline-block text-dark-800 hover:text-primary-900 hover:bg-gray-50 dark:text-gray-500 dark-hover:text-white dark-hover:bg-dark-600 rounded font-bold px-2 py-1 transition duration-300 ease-in-out">EN</a>
+              <a className={`inline-block hover:text-primary-900 hover:bg-gray-50 dark-hover:text-white dark-hover:bg-dark-600 rounded font-bold px-2 py-1 transition duration-300 ease-in-out ${language === 'en' ? 'text-primary-900 dark:text-primary-400' : 'text-dark-800 dark:text-gray-500'}`}>EN</a>
             </Link>
           </li>
         </ul>
