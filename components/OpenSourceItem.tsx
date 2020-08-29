819f9f28575b21aac1f9b6f85da5ad283cd9e7ae
Javier Diaz <coderdiaz@gmail.com>
2020-08-28 19:40:48 -0500

    fix: fixed item

--- components/OpenSourceItem.tsx
+++ components/OpenSourceItem.tsx
@@ -7,7 +7,7 @@ interface IOpenSourceItemProps {
 const OpenSourceItem = ({ item }: IOpenSourceItemProps) => {
   return <a href={item.uri} className="px-4 py-2 flex flex-col md:flex-row items-start md:justify-between md:items-center cursor-pointer hover:bg-gray-50 dark-hover:bg-dark-700 rounded mb-2 transition duration-300 ease-in-out">
     <div className="font-display inline-block text-lg dark:text-gray-200 mb-1 md:mb-0">
-      <span className="inline-flex mr-1">{item.icon}</span> {item.name}
+      <span className="inline-flex mr-2">{item.icon}</span> {item.name}
     </div>
     <p className="lg:w-125 text-base text-gray-600 dark:text-gray-600">{item.description}</p>
   </a>
